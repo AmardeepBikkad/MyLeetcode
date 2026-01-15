35eb5ff90b8ecf94ad1f65a461b3978244fd9b94
Amardeep Bikkad <108120017+AmardeepBikkad@users.noreply.github.com>
2026-01-15 12:41:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (86.99%) - LeetSync

--- 1-two-sum/two-sum.swift
+++ 1-two-sum/two-sum.swift
@@ -1,5 +1,6 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        //substract current number in array from target -> if we found substraction == number added in dictionary -> simply return index of them
         var dict = [Int: Int]()
         for (i, num) in nums.enumerated() {
             if let foundedNumber = dict[target - num] { 
