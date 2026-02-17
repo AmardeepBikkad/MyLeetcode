29930b74f40566055ec411488bf06c673e84f419
Amardeep Bikkad <108120017+AmardeepBikkad@users.noreply.github.com>
2026-02-17 20:07:03 +0530

    Time: 16 ms (36.60%) | Memory: 22.7 MB (11.11%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.swift
+++ 287-find-the-duplicate-number/find-the-duplicate-number.swift
@@ -1,10 +1,14 @@
 class Solution {
     func findDuplicate(_ nums: [Int]) -> Int {
-        let sortedNums = nums.sorted()
-        
-        for i in 0..<sortedNums.count - 1 {
-            if sortedNums[i] == sortedNums[i + 1] {
-                return sortedNums[i]
+        //let sortedNums = nums.sorted()
+        var dict = [Int: Int]()
+        for each in nums {
+            // if sortedNums[i] == sortedNums[i + 1] {
+            //     return sortedNums[i]
+            // }
+            dict[each, default: 0] += 1
+            if dict[each] ?? 0 > 1 {
+                return each
             }
         }
         return 0
