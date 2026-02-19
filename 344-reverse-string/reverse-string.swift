1037f397c6830c0f56eacb0cc6bc8e12596c1bb3
Amardeep Bikkad <108120017+AmardeepBikkad@users.noreply.github.com>
2026-02-19 22:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (32.87%) - LeetSync

---
+++ 344-reverse-string/reverse-string.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        var low = 0, high = s.count - 1 
+        while low < high {
+            let temp = s[low]
+            s[low] = s[high]
+            s[high] = temp
+            low += 1
+            high -= 1
+        }
+    }
+}
