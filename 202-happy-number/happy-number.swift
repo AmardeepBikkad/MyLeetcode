bda1de198de920be206ee288b857b871f36095c6
Amardeep Bikkad <108120017+AmardeepBikkad@users.noreply.github.com>
2026-03-10 23:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (57.86%) - LeetSync

--- 202-happy-number/happy-number.swift
+++ 202-happy-number/happy-number.swift
@@ -1,12 +1,11 @@
 class Solution {
     func isHappy(_ n: Int) -> Bool {
         var slow = n, fast = n
+        //slow as 1 and fast as 2 
         while fast != 1 {
             slow = getSum(slow)
             fast = getSum(fast)
             fast = getSum(fast)
-            print(slow)
-            print(fast)
             if (slow == fast && slow != 1) {
                 return false 
             }
