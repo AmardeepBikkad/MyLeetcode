0300e38f957709940082c6123cd2d40ae4cf528d
Amardeep Bikkad <108120017+AmardeepBikkad@users.noreply.github.com>
2026-06-27 22:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (25.10%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.swift
+++ 88-merge-sorted-array/merge-sorted-array.swift
@@ -1,22 +1,32 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var i = m-1, j = n-1
-        var k = m + n - 1
-        while i >= 0 && j >= 0 {
-            if nums1[i] > nums2[j]  {
-                nums1[k] = nums1[i]
-                i -= 1
+        var result = [Int]()
+        var i = 0, j = 0
+        while (i < m && j < n) {
+            if nums1[i] < nums2[j] {
+                result.append(nums1[i])
+                i+=1
+            } else if nums1[i] > nums2[j] {
+                result.append(nums2[j])
+                j+=1
             } else {
-                nums1[k] = nums2[j]
-                j -= 1
+                result.append(nums1[i])
+                result.append(nums2[j])
+                i+=1
+                j+=1
             }
-            k -= 1
         }
 
-        while j >= 0 {
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+        while i < m {
+            result.append(nums1[i])
+            i+=1
         }
+
+        while j < n {
+            result.append(nums2[j])
+            j+=1
+        }
+        nums1.removeAll()
+        nums1 += result
     }
 }
